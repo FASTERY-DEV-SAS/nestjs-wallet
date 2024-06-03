adff4388f5fa36d7ccb4c2a34859fd5a76b4f923
Joffre Veloz <joffreandres11@gmail.com>
2024-06-03 13:53:44 -0500

    Update categories.controller.ts

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,7 +6,9 @@ import { Auth } from 'src/auth/decorators/auth.decorator';
 import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
