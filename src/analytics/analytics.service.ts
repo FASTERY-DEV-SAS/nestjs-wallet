037e47950c35852cb49c89a3be652925c52c44d7
Joffre Veloz <joffreandres11@gmail.com>
2024-07-19 04:30:04 -0500

    Update analytics.service.ts

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -31,10 +31,12 @@ export class AnalyticsService {
       }
     }
 
-    // Convierte el objeto en un array y ordena por día
+    // Convierte el objeto en un array, divide expense e income por 100, y ordena por día
     const chartData = Object.keys(dailyData).map(day => ({
       day: new Date(day).getDate().toString(), // Extrae solo el día del mes
-      ...dailyData[day],
+      total: dailyData[day].total / 100,
+      expense: dailyData[day].expense / 100,
+      income: dailyData[day].income / 100,
     })).sort((a, b) => a.day.localeCompare(b.day));
 
     return {
